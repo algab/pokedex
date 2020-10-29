8d4ed56f2d134f787f328dcf820d02b616910097
Álvaro Oliveira <alvarogab6@gmail.com>
2020-10-29 19:15:16 -0300

    fix style Home ListPokemons

--- src/screens/Home/List/styles.ts
+++ src/screens/Home/List/styles.ts
@@ -4,17 +4,18 @@ export const ButtonPokemon = styled.TouchableOpacity`
   flex-direction: row;
   justify-content: flex-start;
   align-items: center;
-  margin-bottom: 10px;
   margin-left: 5px;
 `;
 
 export const TextPokemon = styled.Text`
   font-size: 15px;
   text-transform: uppercase;
+  margin-left: 20px;
 `;
 
 export const ImagePokemon = styled.Image`
   height: 80px;
   width: 80px;
   border-radius: 40px;
+  width: 80px;
 `;
